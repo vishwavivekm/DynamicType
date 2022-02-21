321f079a25f654b669394297ce68058fa6898188
VivekM <vishwa.vivekm@gmail.com>
2022-02-21 15:38:47 +0530

    Initial Commit

---
+++ DynamicType/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DynamicType
+//
+//  Created by Vivek on 21/02/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
