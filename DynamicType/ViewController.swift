5c2ff7aff1322590e4791d5ad183ef4e970d2ded
VivekM <vishwa.vivekm@gmail.com>
2022-02-21 18:42:36 +0530

    Initial changes

--- DynamicType/ViewController.swift
+++ DynamicType/ViewController.swift
@@ -11,9 +11,38 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        createView()
     }
 
+    func createView() {
+        let myView = UIView()
+//        view.frame = CGRect(x: 0, y: 0, width: 100, height: 50)
+        
+//        let  const = [
+//            view.centerXAnchor.constraint(equalTo: self.view.centerXAnchor),
+//            view.centerYAnchor.constraint(equalTo: self.view.centerYAnchor),
+//            view.widthAnchor.constraint(equalToConstant: 100),
+//            view.heightAnchor.constraint(equalToConstant: 100)
+//        ]
+////        let horiConst = NSLayoutConstraint(item: view, attribute: NSLayoutConstraint.Attribute.centerX, relatedBy: NSLayoutConstraint.Relation.equal, toItem: view, attribute: NSLayoutConstraint.Attribute.centerX, multiplier: 1, constant: 0)
+////        view.addConstraint(const)
+//        NSLayoutConstraint.activate(const)
+        myView.backgroundColor = .red
+        view.addSubview(myView)
+        
+        myView.translatesAutoresizingMaskIntoConstraints = false
+        
+        myView.centerXAnchor.constraint(equalTo: view.centerXAnchor, constant: 100).isActive = true
+//        myView.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+//        myView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 110).isActive = true
+//        myView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -110).isActive = true
+        myView.topAnchor.constraint(equalTo: view.topAnchor, constant: 200).isActive = true
+        myView.heightAnchor.constraint(equalToConstant: 100).isActive = true
+        myView.widthAnchor.constraint(equalToConstant: 100).isActive = true
+        
+        
+        
+    }
 
 }
 
